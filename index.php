1a08f6fbb72b39fd59619b5abe9db80b38551349
Malcolm Littlefield <mhlittlefield@gmail.com>
2017-06-19 18:23:01 -0400

    created test page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Anastasia Elliott</title>
+<meta charset="utf-8"/>
+</head>
+<body>
+<h1>Anastasia Elliott</h1>
+<p>This is Anastasia Elliott's personal website</p>
+<p>How about we start this site out as basically a digital resume?</p>
+<p>Send me your resume and I will webify it! It will be</p>
+<ul>
+<li>cool</li>
+<li>awesome</li>
+<li>so amazing</li>
+</ul>
+</body>
+</html>
